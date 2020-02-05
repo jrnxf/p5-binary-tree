e71d650e8dfb94ac7cae43fd6951223762cbd4a7
Colby Thomas <coloradocolby@gmailcom>
2020-02-04 23:29:55 -0600

    +

--- sketch.js
+++ sketch.js
@@ -1,19 +1,17 @@
 function setup() {
-  createCanvas(10000, 10000);
-	stroke('#ffffff')
-  background("#43AEDD");
+  createCanvas(windowWidth, windowHeight);
+  stroke('#ffffff');
+  background('#43AEDD');
   const tree = new Tree();
 
   tree;
 
   console.log(tree);
 
-  document.getElementById("insertNode").addEventListener("submit", e => {
+  document.getElementById('insertNode').addEventListener('submit', e => {
     e.preventDefault();
     tree.insert(new Node(parseInt(e.target.node.value, 10)));
     tree.bfs();
-//    e.target.node.value = null;
+    //    e.target.node.value = null;
   });
-	
-window.scrollTo(5000,0)
 }
